a718f183409211c87aa857c0952e4ff15ba969b8
wildbipin <98609629+wildbipin@users.noreply.github.com>
2022-01-28 23:20:11 +0530

    Update configurenginx.sh

--- configurenginx.sh
+++ configurenginx.sh
@@ -1,10 +1,13 @@
-#!/bin/bash
-
 # Update apt cache.
-sudo apt-get update
+#sudo apt-get update
 
 # Install Nginx.
-sudo apt-get install -y nginx
+#sudo apt-get install -y nginx
 
 # Set the home page.
+#echo "<html><body><h2>Welcome to Azure! My name is bipindevops.</h2></body></html>" | sudo tee -a /var/www/html/index.html
+
+#! /bin/bash
+sudo apt-get update
+sudo apt-get install -y nginx
 echo "<html><body><h2>Welcome to Azure! My name is bipindevops.</h2></body></html>" | sudo tee -a /var/www/html/index.html
